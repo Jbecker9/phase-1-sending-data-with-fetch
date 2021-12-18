0525561dc2095cbeaf39dc2bc9649fc922e01635
Jared <jbeckersalax9@aol.com>
2021-12-18 14:26:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+// 
+
+
+// const fromData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+// // const configurationObject = {
+// //     method: "POST",
+// //     headers: {
+// //         "Content-Type": "application/json",
+// //         Accept: "applecation/json",
+// //     },
+// //     body: JSON.stringify({formData}),
+// };
+
+// fetch ("http://localhost:3000/dogs", configurationObject)
+    // .then((resp) => resp.json()).then(function(object){
+    //     console.log(object);
+    // });
+
+function submitData(){
+    const objConfig = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+    })};
+
+    fetch("http://localhost:3000/users", objConfig).then((resp) => resp.json()).then((obj) => submitData(obj))
+}
